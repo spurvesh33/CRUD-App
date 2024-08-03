81f8fe26900db0fda5340838f7e5fd1c07646dcc
Purvesh Salunkhe <salunkhepurvesh258@gmail.com>
2024-08-03 10:23:28 +0530

    Server side

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import SimpleCard from "./components/SimpleCard"
+import SimpleCard from "./components/SimpleCard";
 
 
 
